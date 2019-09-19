b25b2b56e10cc826684dbdc359e118bee950f9f0
wangko27 <845885222@qq.com>
2019-09-19 09:55:35 +0800

    Init Spring boot Project

---
+++ src/main/java/com/xeiot/iot/open/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.xeiot.iot.open.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * Created by wangkun23 on 2019/9/19.
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xeiot/iot/open/demo/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.xeiot.iot.open.demo.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * web环境配置
+ * Created by wangkun23 on 2019/9/19.
+ */
+public class WebConfig implements WebMvcConfigurer {
+}
